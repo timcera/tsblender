9152a1abb81d10461623620cba95c519e45bbad3
Tim Cera <tim@cerazone.net>
2024-01-19 01:12:32 -0500

    test: reworked tests so they operate in temporary directories

--- tests/test_hydrologic_indices.py
+++ tests/test_hydrologic_indices.py
@@ -1,57 +1,67 @@
 import os
-import unittest
+import shutil
 from io import StringIO
+from pathlib import Path
 
 import pandas as pd
+import pytest
 from pandas.testing import assert_frame_equal, assert_series_equal
 
 from tsblender import tsblender
 
 
-class TestSeriesCompare(unittest.TestCase):
-    def setUp(self):
-        os.chdir(os.path.dirname(os.path.abspath(__file__)))
-        tsblender.run("tsproc_hydrologic_indices.inp")
+@pytest.fixture(scope="session")
+def tmppath(tmp_path_factory):
+    print(dir(tmp_path_factory))
+    tmp_file_path = tmp_path_factory.getbasetemp()
+    yield tmp_file_path
+    print("Removing tmp_path directory...")
+    if tmp_file_path.exists():
+        shutil.rmtree(tmp_file_path)
 
-    def test_hydrologic_indices_compare(self):
-        os.chdir(os.path.dirname(os.path.abspath(__file__)))
-        with open("data_out__hydrologic_indices.txt") as f:
-            text = "\n".join([line for line in f if line[:3] == "   "])
 
-        tsblender_series = pd.read_csv(
-            StringIO(text),
-            index_col=0,
-            parse_dates=True,
-            sep=r"  \s+",
-            header=None,
-        )
-        tsblender_series = tsblender_series.reset_index()
-        tsblender_series["index"] = tsblender_series.iloc[:, 0].str.split(":").str[0]
-        tsblender_series = tsblender_series.set_index("index")
-        tsblender_series = tsblender_series[
-            ~tsblender_series.index.duplicated(keep="first")
-        ]
-        print(tsblender_series)
+def test_hydrologic_indices_compare(tmppath):
+    tpath = Path(tmppath) / "test"
+    shutil.copytree(os.path.dirname(os.path.abspath(__file__)), tpath)
+    os.chdir(tpath)
+    tsblender.run("tsproc_hydrologic_indices.inp")
+    with open("data_out__hydrologic_indices.txt") as f:
+        text = "\n".join([line for line in f if line[:3] == "   "])
 
-        with open("tsproc_out/data_out__hydrologic_indices.txt") as f:
-            text = "\n".join([line for line in f if line[:3] == "   "])
+    tsblender_series = pd.read_csv(
+        StringIO(text),
+        index_col=0,
+        parse_dates=True,
+        sep=r"  \s+",
+        header=None,
+    )
+    tsblender_series = tsblender_series.reset_index()
+    tsblender_series["index"] = tsblender_series.iloc[:, 0].str.split(":").str[0]
+    tsblender_series = tsblender_series.set_index("index")
+    tsblender_series = tsblender_series[
+        ~tsblender_series.index.duplicated(keep="first")
+    ]
+    print(tsblender_series)
 
-        tsproc_series = pd.read_csv(
-            StringIO(text),
-            index_col=0,
-            parse_dates=True,
-            sep=r"  \s+",
-            header=None,
-        )
-        tsproc_series = tsproc_series.reset_index()
-        tsproc_series["index"] = tsproc_series.iloc[:, 0].str.split(":").str[0]
-        tsproc_series = tsproc_series.set_index("index")
-        tsproc_series = tsproc_series[~tsproc_series.index.duplicated(keep="first")]
-        joined = tsblender_series.merge(tsproc_series, on="index", how="inner")
+    with open("tsproc_out/data_out__hydrologic_indices.txt") as f:
+        text = "\n".join([line for line in f if line[:3] == "   "])
 
-        tsblender_series = joined["1_x"].astype(float)
-        tsproc_series = joined["1_y"].astype(float)
+    tsproc_series = pd.read_csv(
+        StringIO(text),
+        index_col=0,
+        parse_dates=True,
+        sep=r"  \s+",
+        header=None,
+    )
+    tsproc_series = tsproc_series.reset_index()
+    tsproc_series["index"] = tsproc_series.iloc[:, 0].str.split(":").str[0]
+    tsproc_series = tsproc_series.set_index("index")
+    tsproc_series = tsproc_series[~tsproc_series.index.duplicated(keep="first")]
+    joined = tsblender_series.merge(tsproc_series, on="index", how="inner")
 
-        assert_series_equal(
-            tsblender_series, tsproc_series, atol=0.001, rtol=0.05, check_names=False
-        )
+    tsblender_series = joined["1_x"].astype(float)
+    tsproc_series = joined["1_y"].astype(float)
+
+    assert_series_equal(
+        tsblender_series, tsproc_series, atol=0.001, rtol=0.05, check_names=False
+    )
--- tests/test_series_compare.py
+++ tests/test_series_compare.py
@@ -1,80 +1,97 @@
 import os
+import shutil
 import unittest
+from pathlib import Path
 
 import pandas as pd
+import pytest
 from pandas.testing import assert_frame_equal, assert_series_equal
 
 from tsblender import tsblender
 
 
-class TestSeriesCompare(unittest.TestCase):
-    def setUp(self):
-        os.chdir(os.path.dirname(os.path.abspath(__file__)))
-        tsblender.run("tsproc_series_compare.inp")
+@pytest.fixture(scope="session")
+def tmppath(tmp_path_factory):
+    print(dir(tmp_path_factory))
+    tmp_file_path = tmp_path_factory.getbasetemp()
+    yield tmp_file_path
+    print("Removing tmp_path directory...")
+    if tmp_file_path.exists():
+        shutil.rmtree(tmp_file_path)
 
-    def test_series_compare(self):
-        os.chdir(os.path.dirname(os.path.abspath(__file__)))
-        tsblender_series = pd.read_csv(
-            "data_out__series_compare.txt",
-            index_col=0,
-            parse_dates=True,
-            skiprows=9,
-            sep=":",
-            header=None,
-        )
 
-        tsproc_series = pd.read_csv(
-            "tsproc_out/data_out__series_compare.txt",
-            index_col=0,
-            parse_dates=True,
-            skiprows=9,
-            sep=":",
-            header=None,
-        )
+def test_a(tmppath):
+    tpath = Path(tmppath) / "test"
+    shutil.copytree(
+        os.path.dirname(os.path.abspath(__file__)), tpath, dirs_exist_ok=True
+    )
+    os.chdir(tpath)
+    tsblender.run("tsproc_series_compare.inp")
 
-        assert_frame_equal(tsblender_series, tsproc_series, rtol=0.01, atol=0.01)
 
-    def test_series_compare_values(self):
-        os.chdir(os.path.dirname(os.path.abspath(__file__)))
-        tsblender_series = pd.read_csv(
-            "data_out__series_compare_inputs.txt",
-            parse_dates=[1],
-            index_col=1,
-            skiprows=4,
-            header=None,
-            sep=r"\s+",
-            comment="T",
-        )
+def test_series_compare(tmppath):
+    tpath = Path(tmppath) / "test"
+    os.chdir(tpath)
+    tsblender_series = pd.read_csv(
+        "data_out__series_compare.txt",
+        index_col=0,
+        parse_dates=True,
+        skiprows=9,
+        sep=":",
+        header=None,
+    )
 
-        print(tsblender_series)
-        print(tsblender_series.dtypes)
-        tsblender_series.loc[:, 0] = tsblender_series.loc[:, 0].str.lower()
-        tsblender_series = tsblender_series.reset_index()
-        tsblender_series = tsblender_series.set_index([0, 1]).dropna(axis="index")
+    tsproc_series = pd.read_csv(
+        "tsproc_out/data_out__series_compare.txt",
+        index_col=0,
+        parse_dates=True,
+        skiprows=9,
+        sep=":",
+        header=None,
+    )
 
-        tsproc_series = pd.read_csv(
-            "tsproc_out/data_out__series_compare_inputs.txt",
-            parse_dates=[1],
-            index_col=1,
-            skiprows=3,
-            header=None,
-            sep=r"\s+",
-            comment="T",
-        )
+    assert_frame_equal(tsblender_series, tsproc_series, rtol=0.01, atol=0.01)
 
-        tsproc_series.loc[:, 0] = tsproc_series.loc[:, 0].str.lower()
-        tsproc_series = tsproc_series.reset_index()
-        tsproc_series = tsproc_series.set_index([0, 1]).dropna(axis="index")
 
-        test_df = tsblender_series.merge(
-            tsproc_series, left_index=True, right_index=True, how="right"
-        )
-        print(test_df)
-        assert_series_equal(
-            test_df["3_x"],
-            test_df["3_y"],
-            check_index=False,
-            check_names=False,
-            rtol=0.01,
-            atol=0.01,
-        )
+def test_series_compare_values(tmppath):
+    tpath = Path(tmppath) / "test"
+    os.chdir(tpath)
+    tsblender_series = pd.read_csv(
+        "data_out__series_compare_inputs.txt",
+        parse_dates=[1],
+        index_col=1,
+        skiprows=4,
+        header=None,
+        sep=r"\s+",
+        comment="T",
+    )
+
+    tsblender_series.loc[:, 0] = tsblender_series.loc[:, 0].str.lower()
+    tsblender_series = tsblender_series.reset_index()
+    tsblender_series = tsblender_series.set_index([0, 1]).dropna(axis="index")
+
+    tsproc_series = pd.read_csv(
+        "tsproc_out/data_out__series_compare_inputs.txt",
+        parse_dates=[1],
+        index_col=1,
+        skiprows=3,
+        header=None,
+        sep=r"\s+",
+        comment="T",
+    )
+
+    tsproc_series.loc[:, 0] = tsproc_series.loc[:, 0].str.lower()
+    tsproc_series = tsproc_series.reset_index()
+    tsproc_series = tsproc_series.set_index([0, 1]).dropna(axis="index")
+
+    test_df = tsblender_series.merge(
+        tsproc_series, left_index=True, right_index=True, how="right"
+    )
+    assert_series_equal(
+        test_df["3_x"],
+        test_df["3_y"],
+        check_index=False,
+        check_names=False,
+        rtol=0.01,
+        atol=0.01,
+    )
